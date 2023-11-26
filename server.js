a595be22387b14cb8730ae66c2cc0960f0ede5e3
--mulyani017 <--mulyanimulyani8360@gmail.com>
2023-11-27 01:10:50 +0700

    menambahkan file koneksi

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
+const app = express();
+
+//parse application/json
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+app.listen(3000, () => {
+    console.log('server started on port');
+});
